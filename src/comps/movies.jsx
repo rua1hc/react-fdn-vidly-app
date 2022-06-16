2edb68442fa13bf3f1c069852cc88bcd9887d1d8
rua1hc <tatut90@gmail.com>
2022-06-16 16:19:39 +0700

    s3v18 delete btn

--- src/comps/movies.jsx
+++ src/comps/movies.jsx
@@ -13,36 +13,43 @@ class Movies extends Component {
     };
 
     render() {
+        const { length: count } = this.state.movies;
+
+        if (count === 0) return <p>No movies found!</p>;
+
         return (
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Genre</th>
-                        <th>Stock</th>
-                        <th>Rate</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.movies.map((movie) => (
-                        <tr key={movie._id}>
-                            <td>{movie.title}</td>
-                            <td>{movie.genre.name}</td>
-                            <td>{movie.numberInStock}</td>
-                            <td>{movie.dailyRentalRate}</td>
-                            <td>
-                                <button
-                                    onClick={() => this.handleDelete(movie)}
-                                    className="btn btn-danger"
-                                >
-                                    Delete
-                                </button>
-                            </td>
+            <React.Fragment>
+                <p>Showing {count} movies in DB.</p>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Genre</th>
+                            <th>Stock</th>
+                            <th>Rate</th>
+                            <th></th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {this.state.movies.map((movie) => (
+                            <tr key={movie._id}>
+                                <td>{movie.title}</td>
+                                <td>{movie.genre.name}</td>
+                                <td>{movie.numberInStock}</td>
+                                <td>{movie.dailyRentalRate}</td>
+                                <td>
+                                    <button
+                                        onClick={() => this.handleDelete(movie)}
+                                        className="btn btn-danger"
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </React.Fragment>
         );
     }
 }
