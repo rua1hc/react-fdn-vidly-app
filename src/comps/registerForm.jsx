20d9979a3aca2ffbcb8e89bd0f9981032aed4bb5
rua1hc <tatut90@gmail.com>
2022-07-04 16:21:15 +0700

    s9v9 autologin user upon registration

--- src/comps/registerForm.jsx
+++ src/comps/registerForm.jsx
@@ -3,7 +3,7 @@ import Joi from "joi-browser";
 import { toast } from "react-toastify";
 
 import Form from "./common/form";
-import { registerUser } from "../services/userService";
+import { register } from "../services/userService";
 
 class RegisterForm extends Form {
     state = {
@@ -25,7 +25,9 @@ class RegisterForm extends Form {
         const user = { ...this.state.data };
 
         try {
-            await registerUser(user);
+            const { headers } = await register(user);
+            localStorage.setItem("token", headers["x-auth-token"]);
+            this.props.history.push("/");
         } catch (ex) {
             if (ex.response && ex.response.status === 400) {
                 const errors = { ...this.state.errors };
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,7 @@ import config from "../config.json";
 
 const apiEndpoint = config.apiUrl + "/users";
 
-export function registerUser(user) {
+export function register(user) {
     // const {username:email, password, name} = user;
     return http.post(apiEndpoint, {
         email: user.username,
